17a8be1c56c807b952d57d1a6d4d599f8127397d
Patryk Grochowski <the.pgg@gmail.com>
2015-03-14 01:18:00 -0700

    Added a crappy octave implementation

---
+++ pi.m
@@ -0,0 +1,24 @@
+#!/usr/bin/octave -qf
+% Octave implementation of Leibniz formula for pi
+% Octave complains about the name of the file shadowing a built-in function
+% but we're not using that function, we're calculating our own pi value~
+
+arg_list = argv();
+
+if (nargin < 1)
+    printf("Usage: %s <iterations>\n", program_name);
+    exit;
+endif
+
+sum = 0.0;
+iter = str2double(arg_list{1});
+index = 0;
+
+printf("Doing %d iterations.\n", iter);
+
+for(index = 0:1:iter)
+    sum = sum + 2.0/((4*index+1)*(4*index+3));
+endfor
+
+printf("The calculated value of pi is %.21f\n", (4.0*sum));
+exit;
